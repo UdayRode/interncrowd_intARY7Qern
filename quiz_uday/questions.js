b63ecb3e297a203f3c3518a325249ac7ac1f322c
UdayRode <125876408+UdayRode@users.noreply.github.com>
2023-03-02 18:04:32 +0530

    Add files via upload

---
+++ quiz_uday/questions.js
@@ -0,0 +1,59 @@
+
+let questions = [
+    {
+    numb: 1,
+    question: "BORDER attributes specifies the",
+    answer: " thickness of the border around the table",
+    options: [
+      " thickness of the border around the table",
+      "space between two borders",
+      "space between two cells",
+      "width of the table"
+    ]
+  },
+    {
+    numb: 2,
+    question: "Which control is used when the input from the user may extend to several lines?",
+    answer: "multiline text area",
+    options: [
+      "multiline text area",
+      "check box",
+      "password control",
+      "menu control"
+    ]
+  },
+    {
+    numb: 3,
+    question: "The NOSHADE attribute ______",
+    answer: "displays the line in dark gray",
+    options: [
+      "displays the example in red",
+      "displays the line in dark gray",
+      "displays the line in red",
+      "defines the thickness of the line"
+    ]
+  },
+    {
+    numb: 4,
+    question: "When the mouse is clicked on a link, it is called as",
+    answer: " an active link",
+    options: [
+      "a default link",
+      " an active link",
+      "a link",
+      "a event"
+    ]
+  },
+    {
+    numb: 5,
+    question: "The <BIG> tag is used",
+    answer: "to display text in a font larger than the font currently being used",
+    options: [
+      "to display text in a font larger than the font currently being used",
+      "to display text in a font with less size",
+      " to display text in a particular colour",
+      "to display text"
+    ]
+  },
+
+];
